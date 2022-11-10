3c5210339661a30c5bd4fc04c3e801b506c1f4e0
Carlos Paniagua <cpaniaguam@gmail.com>
2022-11-10 10:58:39 -0500

    landmask structured

--- workflow/scripts/apply-landmask.jl
+++ workflow/scripts/apply-landmask.jl
@@ -1,14 +1,18 @@
 
-using IceFloeTracker: create_landmask, apply_landmask, load, readdlm
+# using IceFloeTracker: create_landmask, apply_landmask, load
+using DelimitedFiles: readdlm
 dir = ARGS[1]
 strel = readdlm("se_landmask.csv", ',', Bool)
-imgs = readdir(dir)
-for i in imgs
+imgs = readdir(dir); total = length(imgs)
+for (i,img) in enumerate(imgs)
+    fname = img[1:findlast(".",img)[1]-1]
+    @info "Processing $img ($i/$total)"
     # create landmask and apply it
     # img = load(i)
-    img = println(i)
+    # img = println(i)
     # landmask = create_landmask(img, se)
-    println("creating landmask")
-    println("applying landmask and saving to", "$i"*"_landmasked" )
+    @info "creating landmask for $img"
+    @info "applying landmask and saving to $fname"*"_landmasked"
+    println()
     # @persist apply_landmask(img, landmask) "$i"*"_landmasked"
 end
