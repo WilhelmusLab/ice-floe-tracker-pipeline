e91e11f78ffb4b31239cb22a454932fe7e953341
Carlos Paniagua <cpaniaguam@gmail.com>
2022-11-11 14:50:07 -0500

    fix: set up file structure for results - create /result for output - look for `Land.tif` in resources/input_images - handle not coastline image - TODO: use approapiate functions

--- workflow/scripts/apply-landmask.jl
+++ workflow/scripts/apply-landmask.jl
@@ -1,23 +1,49 @@
 #!/usr/bin/env julia
-# using IceFloeTracker: create_landmask, apply_landmask, load, persist
-using DelimitedFiles: readdlm
+using IceFloeTracker: create_landmask, apply_landmask, load, @persist
+# using DelimitedFiles: readdlm
+
+# @time begin
 dir = ARGS[1]
-strel = readdlm("se_landmask.csv", ',', Bool)
-if !isdir("landmasked_images")
-    mkdir("landmasked_images")
+dir = "resources/input-images"
+# se = readdlm("se_landmask.csv", ',', Bool)
+# se = readdlm("se_landmask.csv", ',', Bool)
+targetdir = "results"
+
+targetsubdir = joinpath(targetdir,"landmasked_images")
+pwd()
+
+if !isdir(targetsubdir)
+    @info "making directory at $targetsubdir"
+    mkdir(targetsubdir)
+else
+    @info "say what to do if directory already exists."
 end
+
 imgs = readdir(dir); total = length(imgs)
-for (i,img) in enumerate(imgs)
-    fname = img[1:findlast(".",img)[1]-1]*"_landmasked"
-    path = joinpath("landmasked_images",fname)
-    @info "Processing $img ($i/$total)"
+
+# look for Land.tif and remove it
+    if "Land.tif" in imgs
+        @info "`Land.tif` found in $dir. Using it as coastline image for masking land."
+    else
+        error("`Land.tif` not found in $dir. Please ensure a coastline image `Land.tif` exists in $dir.")
+    end
+
+    deleteat!(imgs, findall(x->x=="Land.tif",imgs))
+
+# Use a subset for now
+    imgs = imgs[1:2]
+
+# for (i, imgname) in enumerate(imgs)
+    i=1; imgname = imgs[1]
+    fname = imgname[1:findlast(".",imgname)[1]-1]*"_landmasked.png"
+    outpath = joinpath(targetsubdir, fname)
+    @info "Processing $imgname ($i/$total)"
     # create landmask and apply it
-    # img = load(i)
+    @time img = load(joinpath(dir,imgname))
     # img = println(i)
-    # landmask = create_landmask(img, se)
-    @info "creating landmask for $img"
-    @info "applying landmask and saving to $fname"
-    println()
-    # @persist landmasked = apply_landmask(img, landmask) "$i"*"_landmasked"
-    # Apply landmask?
-end
+    @info "creating landmask for $imgname"
+    # @time landmask = create_landmask(img)
+    @info "applying landmask and persisting to $fname in $targetsubdir"
+    @persist landmasked = apply_landmask(img, landmask) outpath
+# end
+# end
