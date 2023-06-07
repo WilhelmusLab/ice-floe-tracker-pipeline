5bb049ce307df3ceda09e27e9c56529c81faefaa
Timothy Divoll <timothy.divoll@gmail.com>
2023-06-07 12:39:00 -0400

    test: soit path resolution

--- workflow/scripts/pass_time_cylc.py
+++ workflow/scripts/pass_time_cylc.py
@@ -45,7 +45,7 @@ def main():
     startdate = os.environ.get("startdate")
     enddate = os.environ.get("enddate")
     csvdir = os.environ.get("soit_dir")
-    csvoutpath = "/tmpdownload"
+    csvoutpath = "/IceFloeTracker.jl/None"
     #csvoutpath = f'{ProjectHome}/{csvdir}'
     centroidx = os.environ.get("centroid_x")
     centroidy = os.environ.get("centroid_y")
