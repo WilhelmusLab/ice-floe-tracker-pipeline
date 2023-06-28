0323a42302b4aeed72e9ee1bb036063e2199cef6
Carlos Paniagua <cpaniaguam@gmail.com>
2023-06-28 09:58:53 -0400

    feat: add Python deps for lat/lon mapping numpy, rasterio, pyproj

--- src/IFTPipeline.jl
+++ src/IFTPipeline.jl
@@ -11,12 +11,24 @@ using IceFloeTracker: Folds, RGB, Gray, load, float64, imsharpen
 using Dates
 using DataFrames
 using TOML: parsefile
+using PyCall
 include("cli-config.jl")
 include("soit-parser.jl")
 include("landmask.jl")
 include("preprocess.jl")
 include("feature-extraction.jl")
 include("tracker.jl")
+
+const np = PyNULL()
+const pyproj = PyNULL()
+const rasterio = PyNULL()
+
+function __init__()
+    copy!(np, pyimport_conda("numpy", "numpy=1.25.0"))
+    copy!(pyproj, pyimport_conda("pyproj", "pyproj=3.6.0"))
+    copy!(rasterio, pyimport_conda("rasterio", "rasterio=1.3.7"))
+end
+
 export cache_vector, sharpen,
     sharpen_gray,
     preprocess,
