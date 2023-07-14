23abea967afb3c1ad518df080baa34b1c1684218
Carlos Paniagua <cpaniaguam@gmail.com>
2023-07-14 15:18:03 -0400

    fix: remove numpy from deps check

--- test/test-pydeps.jl
+++ test/test-pydeps.jl
@@ -1,5 +1,5 @@
 @testset "python deps" begin
-    depslatlon = ["numpy", "pyproj", "rasterio"]
+    depslatlon = ["pyproj", "rasterio"] # numpy already included in skimage
     depssoit = ["requests", "skyfield"]
     deps = vcat(depslatlon, depssoit)
     pkgs = Conda._installed_packages()
