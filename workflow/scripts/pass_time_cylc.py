72c6ea6d17e038fa33dcff7a9e64ac50d9b54282
Timothy Divoll <timothy.divoll@gmail.com>
2023-07-27 20:41:47 -0400

    test: add end and start env vars for argparse in passtimes script

--- workflow/scripts/pass_time_cylc.py
+++ workflow/scripts/pass_time_cylc.py
@@ -43,6 +43,8 @@ class MyError(Exception):
 def get_passtimes(
     startdate, enddate, csvoutpath, centroid_x, centroid_y, SPACEUSER, SPACEPSWD
 ):
+    enddate = enddate
+    startdate = startdate
     centroidx = centroid_x
     centroidy = centroid_y
     configUsr = SPACEUSER
