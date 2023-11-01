bf7ca833f7ca89e341c6990a22c5f580da6b537b
Timothy Divoll <timothy.divoll@gmail.com>
2023-11-01 14:29:28 -0400

    feat: add script to read csv and generate a flow.cylc template

--- workflow/scripts/flow_generator.py
+++ workflow/scripts/flow_generator.py
@@ -1,60 +1,60 @@
 
 from jinja2 import Environment, FileSystemLoader # templating engine
-import csv, os, yaml # parser
+import pandas as pd, os
 
-input_csv_file = "./config/site_locations.csv"
-output_yaml_file = "./config/site_data.yaml"
+def get_parameters(data, name, crs=None):
+    if name != "bounding_box":
+        return ",".join(list(data[name].values.astype(str)))
+    elif name == "bounding_box" and crs is not None:
+        return get_bounding_box_list(data, crs)
+    
+def generate_bounding_box(row, crs):
+    if crs == "epsg3413":
+        columns = ["left_x", "top_y", "right_x", "lower_y"]
+    elif crs == "wgs84":
+        columns = ["top_left_lat", "top_left_lon", "lower_right_lat", "lower_right_lon"]
+    return "@".join(list(row[columns].values.astype(str)))
 
-def convert_csv_to_yaml():
-    """
-    Convert the CSV matrix 'site_locations.csv' into a yaml file.    
-    """
-    data = {}
-    with open(input_csv_file, 'r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for row in csv_reader:
-            for column, value in row.items():
-                if column not in data:
-                    data[column] = []
-                data[column].append(value)
+def get_bounding_box_list(data, crs="wgs84"):
+    f = lambda x: generate_bounding_box(x, crs) 
+    return ",".join(data.apply(f, axis=1).values)
 
-    # Write the data to a YAML file
-    with open(output_yaml_file, 'w') as yaml_file:
-        yaml.dump(data, yaml_file, default_flow_style=False)
-
-    print(f"CSV file '{input_csv_file}' has been converted to YAML file '{output_yaml_file}'.")
-
-def parse_paramsyml(yamlfile):
-    """
-    Parse yaml file with parameters for cylc files.    
-    """
-    with open(yamlfile, "r") as f:
-        dataimp = yaml.load(f, Loader=yaml.FullLoader)
-    n = len(dataimp["startdate"])
-    return [{k: dataimp[k][i] for k in dataimp} for i in range(n)]
-
-def generate_cylc_files(envfile, template, template_dir="./config"):
+def generate_cylc_file(csvfile="site_locations.csv", template="flow_template.j2", template_dir="./config", crs="wgs84", minfloearea=350, maxfloearea=90_000):
     """
     Generate cylc files from template.
 
     Parameters
     ----------
-    envfile : str
-        Path to yaml file with parameters for cylc files.
+    csvfile : str
+        Name of CSV file with parameters for cylc flow file. Each row is a unique set of parameters.
     template : str
-        Name of template file.
+        Name of template file to fill.
     template_dir : str
-        Path to directory with template file.
+        Path to directory with template and csv files.
+    crs : str
+        Either "wgs84" (default) or "epsg3413"
+    minfloearea : int
+        Default = 350
+    maxfloearea : int
+        Default = 90,000
     """
-    convert_csv_to_yaml()
-    data = parse_paramsyml(envfile)
+    df = pd.read_csv(csvfile)
+    df['startdate'] = pd.to_datetime(df['startdate']).dt.strftime('%Y-%m-%d')
+    df['enddate'] = pd.to_datetime(df['enddate']).dt.strftime('%Y-%m-%d')
+    df['bounding_box'] = None
+    data = {c: get_parameters(df, c, crs) for c in df.columns}
+    data['crs'] = crs
+    data['minfloearea'] = minfloearea
+    data['maxfloearea'] = maxfloearea
+    data['centroid_x'] = data['center_x']
+    data['centroid_y'] = data['center_y']
+    template_dir = "./config"
+    template = "flow_template.j2"
     env = Environment(loader=FileSystemLoader(template_dir))
     template = env.get_template(template)
-    fname = os.path.join(template_dir, "flow.cylc")
-    print(data)
-    for i, conf in enumerate(data, 1):
-        content = template.render(conf)
-        with open(fname, "w") as fh:
-            fh.write(content)
+    fname = os.path.join(template_dir, "testflow.cylc")
+    content = template.render(data)
+    with open(fname, "w") as fh:
+        fh.write(content)
 
-generate_cylc_files('./config/site_data.yaml', 'flow_template.j2', template_dir='./config')
+generate_cylc_file('./config/site_locations.csv', 'flow_template.j2', template_dir='./config', crs="wgs84")
