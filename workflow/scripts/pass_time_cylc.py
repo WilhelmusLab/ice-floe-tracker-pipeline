8b7952efd0dbb2b6bd2c2b1183594ea76547b6e0
Carlos Paniagua <cpaniaguam@gmail.com>
2024-08-21 17:44:47 -0400

    refactor: optimize pass time calculation loop

--- workflow/scripts/pass_time_cylc.py
+++ workflow/scripts/pass_time_cylc.py
@@ -77,266 +77,27 @@ def get_passtimes(
     rows = []
 
     # Loop through each day until the end date of interest is reached.
-    while True:
+    while not np.array_equiv(tomorrow, end_date):
         # Get UTC time values of the start of today and the start of tomorrow.
         # Passes between these times are considered.
-        t0 = ts.utc(int(today[2]), int(today[0]), int(today[1]))
-        t1 = ts.utc(int(tomorrow[2]), int(tomorrow[0]), int(tomorrow[1]))
+        t0 = to_utc(today)
+        t1 = to_utc(tomorrow)
 
-        # Load in aqua TLE from space track data.
-        aqua_tleline1 = aquaData[aqua_i]["TLE_LINE1"]
-        aqua_tleline2 = aquaData[aqua_i]["TLE_LINE2"]
-
-        # Create new aqua satellite with this TLE.
+        min_diff_index, closest_aqua_epoch_to_t0 = getclosestepoch(t0, aquaData)
+        aqua_tleline1, aqua_tleline2 = get_tli_lines(aquaData[min_diff_index])
         aqua = EarthSatellite(aqua_tleline1, aqua_tleline2, "AQUA", ts)
 
-        # Load in terra TLE from space track data.
-        terra_tleline1 = terraData[terra_i]["TLE_LINE1"]
-        terra_tleline2 = terraData[terra_i]["TLE_LINE2"]
-
-        # Create new terra satellite with this TLE.
+        min_diff_index, closest_terra_epoch_to_t0 = getclosestepoch(t0, terraData)
+        terra_tleline1, terra_tleline2 = get_tli_lines(terraData[min_diff_index])
         terra = EarthSatellite(terra_tleline1, terra_tleline2, "TERRA", ts)
 
-        # Update aqua index. If the epoch of the current TLE is more than a day away from the area of interest,
-        # go to a newer TLE. This keeps the TLE epoch close to the date of the pass, decreasing error in the orbital
-        # mechanics algorithm.
-        while (t0 - aqua.epoch) > 1:
-            aqua_i += 1
-            aqua_tleline1 = aquaData[aqua_i]["TLE_LINE1"]
-            aqua_tleline2 = aquaData[aqua_i]["TLE_LINE2"]
-            aqua = EarthSatellite(aqua_tleline1, aqua_tleline2, "AQUA", ts)
-
-        # Update terra index.
-        while (t0 - terra.epoch) > 1:
-            terra_i += 1
-            terra_tleline1 = terraData[terra_i]["TLE_LINE1"]
-            terra_tleline2 = terraData[terra_i]["TLE_LINE2"]
-            terra = EarthSatellite(terra_tleline1, terra_tleline2, "TERRA", ts)
-
-        # Find times in the time interval of interest where aqua's position in the sky passes over 30º for an observer
-        # in the area of interest, i.e. when it's passing by relatively close.
-        aqua_t, aqua_events = aqua.find_events(
-            aoi, t0, t1, altitude_degrees=30.0)
-
-        # Create empty arrays of today's passes and a dict for each pass.
-        todays_aqua_passes = []
-        aqua_passdict = {}
-
-        # Loop through all Aqua events.
-        for i in range(len(aqua_events)):
-            # Get first event type and time.
-            event = aqua_events[i]
-            ti = aqua_t[i]
-
-            # If this is the first event in the list and it's of type "overpass", this means that the rise of this
-            # pass occurred on the previous day. This is an edge case when the area of interest is near the
-            # international date line. The rise location values are set to nan, since these aren't necessary or easy to
-            # retrieve in this case. The location of the satellite at the overpass point, the overpass time, and the
-            # distance to the aoi at this point are recorded in the dictionary.
-            if event == 1 and i == 0:
-                aqua_passdict["rise_lat"] = float("nan")
-                aqua_passdict["rise_lon"] = float("nan")
-                difference = aqua - aoi
-                topocentric = difference.at(ti)
-                alt, az, distance = topocentric.altaz()
-                aqua_passdict["distance"] = distance.km
-                aqua_passdict["time"] = ti.utc_strftime("%Y %b %d %H:%M:%S")
-
-                geocentric = aqua.at(ti)
-                overlat, overlon = wgs84.latlon_of(geocentric)
-                aqua_passdict["over_lat"] = overlat.degrees
-                aqua_passdict["over_lon"] = overlon.degrees
-
-            # This is another edge case when the first event in the list is of type "set". This occurs when the
-            # satellite rises and passes over on the previous day, then sets today. If the overpass occurred yesterday,
-            # it is already counted yesterday, so this event is ignored.
-            elif event == 2 and i == 0:
-                pass
-
-            # This is an edge case where the final event of the day is an overpass. If so, this means that the satellite
-            # sets on the next day, so the set location is set to nan.
-            elif i == len(aqua_events) and event == 1:
-                difference = aqua - aoi
-
-                topocentric = difference.at(ti)
-
-                alt, az, distance = topocentric.altaz()
-                aqua_passdict["distance"] = distance.km
-                aqua_passdict["time"] = ti.utc_strftime("%Y %b %d %H:%M:%S")
-
-                geocentric = aqua.at(ti)
-                overlat, overlon = wgs84.latlon_of(geocentric)
-                aqua_passdict["over_lat"] = overlat.degrees
-                aqua_passdict["over_lon"] = overlon.degrees
-
-                aqua_passdict["set_lat"] = float("nan")
-                aqua_passdict["set_lon"] = float("nan")
-                todays_aqua_passes.append(aqua_passdict)
-
-            # If the event is a 'rise', save the rise longitude and latitude to the dict representing the pass.
-            elif event == 0:
-                aqua_passdict = {}
-                geocentric = aqua.at(ti)
-                riselat, riselon = wgs84.latlon_of(geocentric)
-                aqua_passdict["rise_lat"] = riselat.degrees
-                aqua_passdict["rise_lon"] = riselon.degrees
-
-            # If the event is an 'overpass', save the overpass longitude and latitude, the time, and the minimum
-            # distance to the aoi to the dict representing the pass.
-            elif event == 1:
-                difference = aqua - aoi
-
-                topocentric = difference.at(ti)
-
-                alt, az, distance = topocentric.altaz()
-                aqua_passdict["distance"] = distance.km
-                aqua_passdict["time"] = ti.utc_strftime("%Y %b %d %H:%M:%S")
-
-                geocentric = aqua.at(ti)
-                overlat, overlon = wgs84.latlon_of(geocentric)
-                aqua_passdict["over_lat"] = overlat.degrees
-                aqua_passdict["over_lon"] = overlon.degrees
-
-            # If the event is a 'set', save the set longitude and latitude to the dict representing the pass.
-            else:
-                geocentric = terra.at(ti)
-                setlat, setlon = wgs84.latlon_of(geocentric)
-                aqua_passdict["set_lat"] = setlat.degrees
-                aqua_passdict["set_lon"] = setlon.degrees
-                todays_aqua_passes.append(aqua_passdict)
-
-        # Assume the minimum distance for a pass is very large until proven otherwise.
-        least_distance = math.inf
-
-        # Time of closest pass.
-        closest_time = ""
-
-        # Loop through each pass in the list of passes.
-        for pass_dict in todays_aqua_passes:
-            # If the rising latitude of the pass is less than the overpass latitude of the pass, this is a ascending
-            # pass, which means a daytime pass for Aqua. These are the only passes of interest. If, furthermore, it's
-            # less than the current minimum distance between the overpass and the aoi, this is the new closest pass.
-            if not np.isnan(pass_dict["rise_lat"]):
-                if (pass_dict["rise_lat"] < pass_dict["over_lat"]) and (
-                    pass_dict["distance"] < least_distance
-                ):
-                    least_distance = pass_dict["distance"]
-                    closest_time = pass_dict["time"]
-
-            # For some edge case passes, rise latitude values are undefined. In this case, compare overpass latitude and
-            # set latitude values. If the overpass latitude is less than the set latitude, it's an ascending pass.
-            else:
-                if (pass_dict["set_lat"] > pass_dict["over_lat"]) and (
-                    pass_dict["distance"] < least_distance
-                ):
-                    least_distance = pass_dict["distance"]
-                    closest_time = pass_dict["time"]
-
-        # Save this closest time as determined above.
-        aqua_closest = closest_time.split(" ")[3]
-
-        # Repeat for Terra.
-        terra_t, terra_events = terra.find_events(
-            aoi, t0, t1, altitude_degrees=30.0)
-
-        todays_terra_passes = []
-        terra_passdict = {}
-
-        # Do the same as above for all terra events.
-        for i in range(len(terra_events)):
-            event = terra_events[i]
-            ti = terra_t[i]
-
-            if event == 1 and i == 0:
-                terra_passdict["rise_lat"] = float("nan")
-                terra_passdict["rise_lon"] = float("nan")
-                difference = terra - aoi
-                topocentric = difference.at(ti)
-                alt, az, distance = topocentric.altaz()
-                terra_passdict["distance"] = distance.km
-                terra_passdict["time"] = ti.utc_strftime("%Y %b %d %H:%M:%S")
-
-                geocentric = terra.at(ti)
-                overlat, overlon = wgs84.latlon_of(geocentric)
-                terra_passdict["over_lat"] = overlat.degrees
-                terra_passdict["over_lon"] = overlon.degrees
-            elif event == 2 and i == 0:
-                pass
-            elif i == len(terra_events) and event == 1:
-                difference = terra - aoi
-
-                topocentric = difference.at(ti)
-
-                alt, az, distance = topocentric.altaz()
-                terra_passdict["distance"] = distance.km
-                terra_passdict["time"] = ti.utc_strftime("%Y %b %d %H:%M:%S")
-
-                geocentric = terra.at(ti)
-                overlat, overlon = wgs84.latlon_of(geocentric)
-                terra_passdict["over_lat"] = overlat.degrees
-                terra_passdict["over_lon"] = overlon.degrees
-
-                terra_passdict["set_lat"] = float("nan")
-                terra_passdict["set_lon"] = float("nan")
-                todays_terra_passes.append(terra_passdict)
-
-            elif event == 0:
-                terra_passdict = {}
-                geocentric = terra.at(ti)
-                riselat, riselon = wgs84.latlon_of(geocentric)
-                terra_passdict["rise_lat"] = riselat.degrees
-                terra_passdict["rise_lon"] = riselon.degrees
-            elif event == 1:
-                difference = terra - aoi
-
-                topocentric = difference.at(ti)
-
-                alt, az, distance = topocentric.altaz()
-                terra_passdict["distance"] = distance.km
-                terra_passdict["time"] = ti.utc_strftime("%Y %b %d %H:%M:%S")
-
-                geocentric = terra.at(ti)
-                overlat, overlon = wgs84.latlon_of(geocentric)
-                terra_passdict["over_lat"] = overlat.degrees
-                terra_passdict["over_lon"] = overlon.degrees
-            else:
-                geocentric = terra.at(ti)
-                setlat, setlon = wgs84.latlon_of(geocentric)
-                terra_passdict["set_lat"] = setlat.degrees
-                terra_passdict["set_lon"] = setlon.degrees
-                todays_terra_passes.append(terra_passdict)
-
-        least_distance = math.inf
-
-        closest_time = ""
-
-        for pass_dict in todays_terra_passes:
-            # If the rising latitude of the pass is greater than the overpass latitude of the pass, this is a descending
-            # pass, which means a daytime pass for Terra. These are the only passes of interest. If, furthermore, it's
-            # less than the current minimum distance between the overpass and the aoi, this is the new closest pass.
-            if not np.isnan(pass_dict["rise_lat"]):
-                if (pass_dict["rise_lat"] > pass_dict["over_lat"]) and (pass_dict["distance"] < least_distance):
-                    least_distance = pass_dict["distance"]
-                    closest_time = pass_dict["time"]
-
-            # For some edge case passes, rise latitude values are undefined. In this case, compare overpass latitude and
-            # set latitude values. If the overpass latitude is greater than the set latitude, it's a descending pass.
-            else:
-                if (pass_dict["set_lat"] < pass_dict["over_lat"]) and (pass_dict["distance"] < least_distance):
-                    least_distance = pass_dict["distance"]
-                    closest_time = pass_dict["time"]
-
-        terra_closest = closest_time.split(" ")[3]
+        aqua_closest, terra_closest = getclosest(aqua, terra, aoi, t0, t1)
 
         # Add closest passes of the day to array of passes.
         rows.append(["-".join(today), aqua_closest, terra_closest])
 
-        # If today is the end date, break, otherwise set the current day to tomorrow.
-        if np.array_equiv(tomorrow, end_date):
-            break
-        else:
-            today = getNextDay(today)
-            tomorrow = getNextDay(today)
+        today = getNextDay(today)
+        tomorrow = getNextDay(today)
 
     csvwrite(start_date, end_date, lat, lon, rows, csvoutpath)
 
