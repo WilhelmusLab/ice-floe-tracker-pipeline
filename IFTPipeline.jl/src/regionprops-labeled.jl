8f35b6029a78c3efd8af833a7fd77b03fad10c19
John Gerrard Holland <john_holland1@brown.edu>
2024-09-20 15:16:36 +0000

    add tests of the cropping with different inputs

--- IFTPipeline.jl/src/regionprops-labeled.jl
+++ IFTPipeline.jl/src/regionprops-labeled.jl
@@ -1,45 +1,69 @@
 using Images
-"""
-    cropfloe(floesimg, props, i)
 
-Crops the floe delimited by the bounding box data in `props` at index `i` from the floe image `floesimg`.
-"""
-function cropfloe(floesimg::Array{Int}, props::DataFrame, i::Int)
-    #= 
-    Crop the floe using bounding box data in props.
-    Note: Using a view of the cropped floe was considered but if there were multiple components in the cropped floe, the source array with the floes would be modified. =#
-    return cropfloe(floesimg, props.min_row[i], props.min_col[i], props.max_row[i], props.max_col[i], props.label[i])
-end
 
+"""
+    cropfloe(floesimg, label)
+
+Crops the floe from `floesimg` with the label `label`, adding a one pixel border of zeros and converting to a BitMatrix.
+"""
 function cropfloe(floesimg::Array{Int}, label::Int)
     #= Remove any pixels not corresponding to that numbered floe 
     (each segment has a different integer) =#
     floe_area = floesimg .== label
     @info "mask: $floe_area"
-    
+
     # Crop the floe to the size of the floe.
-    nonzero = x -> x>0
+    nonzero = x -> x > 0
     rows = 2
     cols = 1
     row_sums = count(floe_area, dims=rows)
     col_sums = count(floe_area, dims=cols)
     @info "row_sums: $row_sums, col_sums: $col_sums"
-    
+
     min_row = findfirst(nonzero, row_sums)[cols]
     min_col = findfirst(nonzero, col_sums)[rows]
     max_row = findlast(nonzero, row_sums)[cols]
     max_col = findlast(nonzero, col_sums)[rows]
     @info "($min_row, $min_col), ($max_row, $max_col)"
-    
+
     floe_area_cropped = floe_area[min_row:max_row, min_col:max_col]
     @info "floe_area_cropped: $floe_area_cropped"
 
-    floe_area_padded = parent(padarray(floe_area_cropped, Fill(0,(1,1))))
+    floe_area_padded = parent(padarray(floe_area_cropped, Fill(0, (1, 1))))
     @info "floe_area_padded: $floe_area_padded"
 
     return BitMatrix(floe_area_padded)
 end
 
+
+"""
+    cropfloe(floesimg, min_row, min_col, max_row, max_col)
+
+Crops the floe delimited by `min_row`, `min_col`, `max_row`, `max_col`, from the floe image `floesimg`.
+"""
+function cropfloe(floesimg::BitMatrix, min_row::Int, min_col::Int, max_row::Int, max_col::Int)
+    #= 
+    Crop the floe using bounding box data in props.
+    Note: Using a view of the cropped floe was considered but if there were multiple components in the cropped floe, the source array with the floes would be modified. =#
+    prefloe = floesimg[min_row:max_row, min_col:max_col]
+
+    #= Check if more than one component is present in the cropped image.
+    If so, keep only the largest component by removing all on pixels not in the largest component =#
+    components = label_components(prefloe, trues(3, 3))
+
+    if length(unique(components)) > 2
+        mask = IceFloeTracker.bwareamaxfilt(components .> 0)
+        prefloe[.!mask] .= 0
+    end
+    return prefloe
+end
+
+
+"""
+    cropfloe(floesimg, min_row, min_col, max_row, max_col, label)
+
+Crops the floe from `floesimg` with the label `label`, returning the region bounded by `min_row`, `min_col`, `max_row`, `max_col`, and converting to a BitMatrix.
+"""
 function cropfloe(floesimg::Array{Int}, min_row::Int, min_col::Int, max_row::Int, max_col::Int, label::Int)
     #= 
     Crop the floe using bounding box data in props.
@@ -51,10 +75,56 @@ function cropfloe(floesimg::Array{Int}, min_row::Int, min_col::Int, max_row::Int
     (each segment has a different integer) =#
     floe_area = prefloe .== label
     @info "mask: $floe_area"
-    
+
     prefloe[floe_area] .= 1
     prefloe[.!floe_area] .= 0
     @info "final prefloe: $prefloe"
 
     return prefloe
 end
+
+
+"""
+    cropfloe(floesimg, props, i)
+
+Crops the floe delimited by the bounding box data in `props` at index `i` from the floe image `floesimg`.
+
+If the dataframe has bounding box data `min_row`, `min_col`, `max_row`, `max_col`, but no `label`, then returns the largest contiguous component.
+
+If the dataframe has bounding box data `min_row`, `min_col`, `max_row`, `max_col`, and a `label`, then returns the component with the label. In this case, `floesimg` must be an Array{Int}.
+
+If the dataframe has only a `label` and no bounding box data, then returns the component with the label, padded by one cell of zeroes on all sides. In this case, `floesimg` must be an Array{Int}.
+
+
+"""
+function cropfloe(floesimg::Union{Array{Int},BitMatrix}, props::DataFrame, i::Int)
+    props_row = props[i, :]
+    colnames = names(props_row)
+    if "min_row" in colnames && "min_col" in colnames && "max_row" in colnames && "max_col" in colnames
+        if "label" in colnames
+            return cropfloe(
+                floesimg,
+                props_row.min_row,
+                props_row.min_col,
+                props_row.max_row,
+                props_row.max_col,
+                props_row.label
+            )
+        else
+            floesimg_bitmatrix = floesimg .> 0
+            return cropfloe(
+                floesimg_bitmatrix,
+                props_row.min_row,
+                props_row.min_col,
+                props_row.max_row,
+                props_row.max_col
+            )
+        end
+    elseif "label" in colnames
+        return cropfloe(floesimg, props_row.label)
+    end
+end
+
+
+
+
--- IFTPipeline.jl/test/test-regionprops-labeled.jl
+++ IFTPipeline.jl/test/test-regionprops-labeled.jl
@@ -1,6 +1,6 @@
 @testset "regionprops-labeled.jl" begin
-    println("-------------------------------------------------")
-    println("-------------- regionprops (labaled) Tests ----------------")
+    println("------------------------------------------------------------")
+    println("-------------- regionprops (labelled) Tests ----------------")
 
     img1 = Int[
         0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0   # row  1
@@ -19,22 +19,162 @@
         0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0   #     14
         0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0   #     15
         0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0   #     16
-    # col
-    #   1 2 3 4 5 6 7 8 9 a b c d e f g
+        #= col
+        1 2 3 4 5 6 7 8 9 a b c d e f g
+        =#
 
     ]
 
-    props = DataFrame(label=Int[], min_row=Int[], min_col=Int[], max_row=Int[], max_col=Int[])
-    push!(props, (1, 1, 1, 4, 4))  
-    push!(props, (2, 1, 4, 5, 8))
-    push!(props, (3, 1, 8, 6, 13))
-    push!(props, (4, 5, 1, 10, 7))
-    push!(props, (5, 5, 1, 10, 7))
+    @info "testing props with label"
+    props_with_label = DataFrame(label=Int[], min_row=Int[], min_col=Int[], max_row=Int[], max_col=Int[])
+    push!(props_with_label, (1, 1, 1, 4, 4))
+    push!(props_with_label, (2, 1, 4, 5, 8))
+    push!(props_with_label, (3, 1, 8, 6, 13))
+    push!(props_with_label, (4, 5, 1, 10, 7))
+    push!(props_with_label, (5, 5, 1, 10, 7))
 
-    @info props[1, :]
 
+    @test cropfloe(img1, props_with_label, 1) == [
+        0 0 0 0
+        0 1 1 0
+        0 1 1 0
+        0 0 0 0
+    ]
+
+    @test cropfloe(img1, props_with_label, 2) == [
+        0 0 0 0 0
+        0 1 1 1 0
+        0 1 1 1 0
+        0 0 1 1 0
+        0 0 0 0 0
+    ]
+
+    @test cropfloe(img1, props_with_label, 3) == [
+        0 0 0 0 0 0
+        0 1 1 1 1 0
+        0 1 1 1 1 0
+        0 1 1 1 1 0
+        0 1 1 1 0 0
+        0 0 0 0 0 0
+    ]
+
+    @test cropfloe(img1, props_with_label, 4) == [
+        0 0 0 0 0 0 0
+        0 1 1 1 1 0 0
+        0 1 0 0 0 0 0
+        0 1 0 0 0 0 0
+        0 1 1 1 1 1 0
+        0 0 0 0 0 0 0
+    ]
+
+    @test cropfloe(img1, props_with_label, 5) == [
+        0 0 0 0 0 0 0
+        0 0 0 0 0 1 0
+        0 0 1 1 1 1 0
+        0 0 1 1 0 0 0
+        0 0 0 0 0 0 0
+        0 0 0 0 0 0 0
+    ]
+
+    @info "testing props without label"
+    props_without_label = DataFrame(min_row=Int[], min_col=Int[], max_row=Int[], max_col=Int[])
+    push!(props_without_label, (1, 1, 4, 4))
+    push!(props_without_label, (1, 4, 5, 8))
+    push!(props_without_label, (1, 8, 6, 13))
+    push!(props_without_label, (5, 1, 10, 7))
+    push!(props_without_label, (5, 1, 10, 7))
+
+    @test cropfloe(img1, props_without_label, 1) == [
+        0 0 0 0
+        0 1 1 0
+        0 1 1 0
+        0 0 0 0
+    ]
+
+    @test cropfloe(img1, props_without_label, 2) == [
+        0 0 0 0 0
+        0 1 1 1 0
+        0 1 1 1 0
+        0 0 1 1 0
+        0 0 0 0 0
+    ]
+
+    @test cropfloe(img1, props_without_label, 3) == [
+        0 0 0 0 0 0
+        0 1 1 1 1 0
+        0 1 1 1 1 0
+        0 1 1 1 1 0
+        0 1 1 1 0 0
+        0 0 0 0 0 0
+    ]
+
+    # This can't distinguish between the two interlocking regions, so it merges them
+    @test cropfloe(img1, props_without_label, 4) == [
+        0 0 0 0 0 0 0
+        0 1 1 1 1 1 0
+        0 1 1 1 1 1 0
+        0 1 1 1 0 0 0
+        0 1 1 1 1 1 0
+        0 0 0 0 0 0 0
+    ]
+
+    @test cropfloe(img1, props_without_label, 4) == cropfloe(img1, props_without_label, 5)
+
+
+
+    @info "testing props with only label"
+    props_only_label = DataFrame(label=Int[])
+    push!(props_only_label, (1,))
+    push!(props_only_label, (2,))
+    push!(props_only_label, (3,))
+    push!(props_only_label, (4,))
+    push!(props_only_label, (5,))
+
+    @test cropfloe(img1, props_only_label, 1) == [
+        0 0 0 0
+        0 1 1 0
+        0 1 1 0
+        0 0 0 0
+    ]
+
+    @test cropfloe(img1, props_only_label, 2) == [
+        0 0 0 0 0
+        0 1 1 1 0
+        0 1 1 1 0
+        0 0 1 1 0
+        0 0 0 0 0
+    ]
+
+    @test cropfloe(img1, props_only_label, 3) == [
+        0 0 0 0 0 0
+        0 1 1 1 1 0
+        0 1 1 1 1 0
+        0 1 1 1 1 0
+        0 1 1 1 0 0
+        0 0 0 0 0 0
+    ]
+
+    @test cropfloe(img1, props_only_label, 4) == [
+        0 0 0 0 0 0 0
+        0 1 1 1 1 0 0
+        0 1 0 0 0 0 0
+        0 1 0 0 0 0 0
+        0 1 1 1 1 1 0
+        0 0 0 0 0 0 0
+    ]
+
+    @test cropfloe(img1, props_only_label, 5) == [
+        0 0 0 0 0 0
+        0 0 0 0 1 0
+        0 1 1 1 1 0
+        0 1 1 0 0 0
+        0 0 0 0 0 0
+    ]
+
+
+    @info "testing values with label"
     # cropfloe
-    @test cropfloe(img1, 1, 1,  4, 4, 1) == [
+    @test cropfloe(img1, 1, 1, 4, 4, 1) == [
         0 0 0 0
         0 1 1 0
         0 1 1 0
@@ -48,41 +188,41 @@
         0 0 1 1 0
         0 0 0 0 0
     ]
-        
+
     @test cropfloe(img1, 1, 8, 6, 13, 3) == [
         0 0 0 0 0 0
         0 1 1 1 1 0
         0 1 1 1 1 0
         0 1 1 1 1 0
         0 1 1 1 0 0
-        0 0 0 0 0 0          
-        ]
-            
+        0 0 0 0 0 0
+    ]
+
     @test cropfloe(img1, 5, 1, 10, 7, 4) == [
         0 0 0 0 0 0 0
         0 1 1 1 1 0 0
         0 1 0 0 0 0 0
         0 1 0 0 0 0 0
         0 1 1 1 1 1 0
-        0 0 0 0 0 0 0        
+        0 0 0 0 0 0 0
     ]
-                
+
     @test cropfloe(img1, 5, 1, 10, 7, 5) == [
         0 0 0 0 0 0 0
         0 0 0 0 0 1 0
         0 0 1 1 1 1 0
         0 0 1 1 0 0 0
-        0 0 0 0 0 0 0 
-        0 0 0 0 0 0 0        
+        0 0 0 0 0 0 0
+        0 0 0 0 0 0 0
     ]
-                  
+
     @test cropfloe(img1, 1) == [
         0 0 0 0
         0 1 1 0
         0 1 1 0
         0 0 0 0
     ]
-            
+
     @test cropfloe(img1, 2) == [
         0 0 0 0 0
         0 1 1 1 0
@@ -90,14 +230,14 @@
         0 0 1 1 0
         0 0 0 0 0
     ]
-    
+
     @test cropfloe(img1, 3) == [
         0 0 0 0 0 0
         0 1 1 1 1 0
         0 1 1 1 1 0
         0 1 1 1 1 0
         0 1 1 1 0 0
-        0 0 0 0 0 0          
+        0 0 0 0 0 0
     ]
 
     @test cropfloe(img1, 4) == [
@@ -106,15 +246,15 @@
         0 1 0 0 0 0 0
         0 1 0 0 0 0 0
         0 1 1 1 1 1 0
-        0 0 0 0 0 0 0        
+        0 0 0 0 0 0 0
     ]
-    
+
     @test cropfloe(img1, 5) == [
         0 0 0 0 0 0
         0 0 0 0 1 0
         0 1 1 1 1 0
         0 1 1 0 0 0
-        0 0 0 0 0 0         
+        0 0 0 0 0 0
     ]
-        
+
 end
