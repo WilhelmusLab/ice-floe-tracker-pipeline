647cd7babd0e42ff5bbb0363c53185002f26a287
John Gerrard Holland <john_holland1@brown.edu>
2024-10-25 14:33:14 -0400

    Update test/test-h5.jl
    Co-authored-by: Carlos Paniagua <cpaniaguam@gmail.com>

--- test/test-h5.jl
+++ test/test-h5.jl
@@ -79,8 +79,8 @@ h5path = joinpath(resdir, "hdf5-files", "20220914T1244.aqua.labeled_image.250m.h
     @test choose_dtype(100) == UInt8
     @test choose_dtype(300) == UInt16
     @test choose_dtype(70_000) == UInt32
-    @test choose_dtype(18_446_744_073_709_551_615) == UInt64
-    @test choose_dtype(18_446_744_073_709_551_616) == UInt128
+    @test choose_dtype(BigInt(2)^64 - 1) == UInt64
+    @test choose_dtype(BigInt(2)^64) == UInt128
     @test choose_dtype(BigInt(2)^128 - 1) == UInt128
     @test_throws "can't be represented" choose_dtype(BigInt(2)^128) 
 end
