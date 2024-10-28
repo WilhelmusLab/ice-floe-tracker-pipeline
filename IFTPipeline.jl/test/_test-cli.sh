46c2089df5bfeac810e75524e2d302d4e3293ce8
John Gerrard Holland <john_holland1@brown.edu>
2024-10-28 19:40:21 +0000

    add example shell script to run the pipeline functions from the command line

---
+++ IFTPipeline.jl/test/_test-cli.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+IFT="julia --project=../ ../src/cli.jl"
+
+DATA_SOURCE=test_inputs/input_pipeline
+TEMPDIR=$(mktemp -d -p __temp__)
+cp ${DATA_SOURCE}/20220914.{terra,aqua}.{true,false}color.250m.tiff ${TEMPDIR}
+cp ${DATA_SOURCE}/landmask.tiff ${TEMPDIR}
+cp ${DATA_SOURCE}/passtimes_lat.csv ${TEMPDIR}
+SAMPLEIMG=${TEMPDIR}/20220914.terra.truecolor.250m.tiff
+
+${IFT} landmask ${TEMPDIR} ${TEMPDIR} --log ${TEMPDIR}
+${IFT} preprocess -t ${TEMPDIR} -r ${TEMPDIR} -l ${TEMPDIR} -p ${TEMPDIR} -o ${TEMPDIR} --log ${TEMPDIR}
+${IFT} extractfeatures -i ${TEMPDIR} -o ${TEMPDIR} --log ${TEMPDIR}
+${IFT} track --imgs ${TEMPDIR} --props ${TEMPDIR} --passtimes ${TEMPDIR} --latlon ${SAMPLEIMG} -o ${TEMPDIR} --log ${TEMPDIR}
+${IFT} makeh5files --pathtosampleimg ${SAMPLEIMG} --resdir ${TEMPDIR} --log ${TEMPDIR}
