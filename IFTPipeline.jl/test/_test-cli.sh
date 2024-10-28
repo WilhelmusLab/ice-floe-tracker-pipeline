445c2bceb2058a4388c6b30a636050e0dff6f7cd
John Gerrard Holland <john_holland1@brown.edu>
2024-10-28 20:53:38 +0000

    switch to using just --debug for logging

--- IFTPipeline.jl/test/_test-cli.sh
+++ IFTPipeline.jl/test/_test-cli.sh
@@ -9,8 +9,8 @@ cp ${DATA_SOURCE}/landmask.tiff ${TEMPDIR}
 cp ${DATA_SOURCE}/passtimes_lat.csv ${TEMPDIR}
 SAMPLEIMG=${TEMPDIR}/20220914.terra.truecolor.250m.tiff
 
-${IFT} landmask ${TEMPDIR} ${TEMPDIR} --log ${TEMPDIR}
-${IFT} preprocess -t ${TEMPDIR} -r ${TEMPDIR} -l ${TEMPDIR} -p ${TEMPDIR} -o ${TEMPDIR} --log ${TEMPDIR}
-${IFT} extractfeatures -i ${TEMPDIR} -o ${TEMPDIR} --log ${TEMPDIR}
-${IFT} track --imgs ${TEMPDIR} --props ${TEMPDIR} --passtimes ${TEMPDIR} --latlon ${SAMPLEIMG} -o ${TEMPDIR} --log ${TEMPDIR}
-${IFT} makeh5files --pathtosampleimg ${SAMPLEIMG} --resdir ${TEMPDIR} --log ${TEMPDIR}
+${IFT} landmask ${TEMPDIR} ${TEMPDIR} --debug
+${IFT} preprocess -t ${TEMPDIR} -r ${TEMPDIR} -l ${TEMPDIR} -p ${TEMPDIR} -o ${TEMPDIR} --debug
+${IFT} extractfeatures -i ${TEMPDIR} -o ${TEMPDIR} --debug
+${IFT} track --imgs ${TEMPDIR} --props ${TEMPDIR} --passtimes ${TEMPDIR} --latlon ${SAMPLEIMG} -o ${TEMPDIR} --debug
+${IFT} makeh5files --pathtosampleimg ${SAMPLEIMG} --resdir ${TEMPDIR} --debug
