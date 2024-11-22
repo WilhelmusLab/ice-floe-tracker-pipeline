3a6491bb77843aa4e156bc06683b7054430b6f77
John Gerrard Holland <john_holland1@brown.edu>
2024-11-22 12:41:07 -0500

    update Original to Lopez in batch file

--- cylc/cylc-batch.sh
+++ cylc/cylc-batch.sh
@@ -1,19 +1,19 @@
 mkdir -p ~/.cylc/flow && cp global.cylc ~/.cylc/flow/.
 
-cylc vip . --set-file example/iftpipeline-test-case.conf -n iftpipeline-test-case-original -s 'PREPROCESSING="Original"'
+cylc vip . --set-file example/iftpipeline-test-case.conf -n iftpipeline-test-case-lopez -s 'PREPROCESSING="Lopez"'
 cylc vip . --set-file example/iftpipeline-test-case.conf -n iftpipeline-test-case-buckley -s 'PREPROCESSING="Buckley"'
 
-cylc vip . --set-file example/beaufort-sea-july.conf -n beaufort-sea-july-original -s 'PREPROCESSING="Original"'
+cylc vip . --set-file example/beaufort-sea-july.conf -n beaufort-sea-july-lopez -s 'PREPROCESSING="Lopez"'
 cylc vip . --set-file example/beaufort-sea-july.conf -n beaufort-sea-july-buckley -s 'PREPROCESSING="Buckley"'
 
-cylc vip . --set-file example/beaufort-sea-march.conf -n beaufort-sea-march-original -s 'PREPROCESSING="Original"'
+cylc vip . --set-file example/beaufort-sea-march.conf -n beaufort-sea-march-lopez -s 'PREPROCESSING="Lopez"'
 cylc vip . --set-file example/beaufort-sea-march.conf -n beaufort-sea-march-buckley -s 'PREPROCESSING="Buckley"'
 
-cylc vip . --set-file example/fram-strait-june.conf -n fram-strait-june-original -s 'PREPROCESSING="Original"'
+cylc vip . --set-file example/fram-strait-june.conf -n fram-strait-june-lopez -s 'PREPROCESSING="Lopez"'
 cylc vip . --set-file example/fram-strait-june.conf -n fram-strait-june-buckley -s 'PREPROCESSING="Buckley"'
 
-cylc vip . --set-file example/fram-strait-april-may-2020.conf -n fram-strait-april-may-2020-original -s 'PREPROCESSING="Original"'
+cylc vip . --set-file example/fram-strait-april-may-2020.conf -n fram-strait-april-may-2020-lopez -s 'PREPROCESSING="Lopez"'
 cylc vip . --set-file example/fram-strait-april-may-2020.conf -n fram-strait-april-may-2020-buckley -s 'PREPROCESSING="Buckley"'
 
-cylc vip . --set-file example/ne-greenland.conf -n ne-greenland-original -s 'PREPROCESSING="Original"'
+cylc vip . --set-file example/ne-greenland.conf -n ne-greenland-lopez -s 'PREPROCESSING="Lopez"'
 cylc vip . --set-file example/ne-greenland.conf -n ne-greenland-buckley -s 'PREPROCESSING="Buckley"'
