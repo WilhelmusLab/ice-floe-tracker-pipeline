55a2238d01a596c2e3ece56be908aee38f70f182
John Gerrard Holland <john_holland1@brown.edu>
2024-11-26 20:02:42 +0000

    fix: force PyCall to use the conda environment on linux

--- PythonSetupForIFTPipeline.jl/setup.jl
+++ PythonSetupForIFTPipeline.jl/setup.jl
@@ -8,5 +8,8 @@ using Conda
 # Add the dependencies to the Conda environment
 Conda.runconda(Conda.Cmd(["env", "update", "-n", "base", "--file", joinpath(@__DIR__, "environment.yaml")]))
 
+# Force PyCall to use the Conda version on Linux.
+ENV["PYTHON"]=""  
+
 # Build PyCall with the new conda environment
 Pkg.build("PyCall")
