650c444139dc986889edf1cd870ba0c2b58859b6
John Gerrard Holland <john_holland1@brown.edu>
2024-11-27 14:31:16 +0000

    Reorder setup to work on first run

--- PythonSetupForIFTPipeline.jl/setup.jl
+++ PythonSetupForIFTPipeline.jl/setup.jl
@@ -1,15 +1,19 @@
-using Pkg
-using Conda
-
 # Instantiate all the dependencies
+using Pkg
 Pkg.instantiate()
 
 # Initialize the Conda environment
-ENV["PYTHON"]=""
-Pkg.build("PyCall")
+using Conda
 
 # Add the dependencies to the Conda environment
-Conda.runconda(Conda.Cmd(["env", "update", "-n", "base", "--file", joinpath(@__DIR__, "environment.yaml")]))
+Conda.runconda(
+    Conda.Cmd([
+        "env", "update", "-n", "base", "--file", joinpath(@__DIR__, "environment.yaml")
+    ]),
+)
 
-# Rebuild PyCall with the new conda environment
+# Force PyCall to use the Conda version on Linux.
+ENV["PYTHON"] = ""
+
+# Build PyCall with the new conda environment
 Pkg.build("PyCall")
