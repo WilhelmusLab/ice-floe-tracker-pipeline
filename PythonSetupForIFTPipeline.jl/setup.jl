a48ba287b0f04fd5fde5370119958bf756437c4e
John Gerrard Holland <john_holland1@brown.edu>
2024-11-27 08:43:14 -0500

    reformat setup.jl

--- PythonSetupForIFTPipeline.jl/setup.jl
+++ PythonSetupForIFTPipeline.jl/setup.jl
@@ -9,7 +9,7 @@ using Conda
 Conda.runconda(Conda.Cmd(["env", "update", "-n", "base", "--file", joinpath(@__DIR__, "environment.yaml")]))
 
 # Force PyCall to use the Conda version on Linux.
-ENV["PYTHON"]=""  
+ENV["PYTHON"] = ""
 
 # Build PyCall with the new conda environment
 Pkg.build("PyCall")
