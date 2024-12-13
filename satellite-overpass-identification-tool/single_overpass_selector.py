645e8daff71b8b5fb5a226cf1771ae1a2a088b16
John Gerrard Holland <john_holland1@brown.edu>
2024-12-13 19:59:39 +0000

    add helper function to pick out a single entry from an overpass csv file

---
+++ satellite-overpass-identification-tool/single_overpass_selector.py
@@ -0,0 +1,43 @@
+import argparse
+import pathlib
+import pandas
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Aqua and Terra Satellite Overpass time tool"
+    )
+    parser.add_argument(
+        type=pathlib.Path,
+        dest="path",
+        help="Date in format YYYY-MM-DD",
+    )
+    parser.add_argument(
+        "--date",
+        type=pandas.to_datetime,
+        dest="date",
+        help="Date in format YYYY-MM-DD",
+    )
+    parser.add_argument(
+        "--satellite",
+        type=str,
+        dest="satellite",
+        help="satellite name in lowercase, e.g. 'aqua', 'terra'",
+    )
+    parser.add_argument(
+        "--field",
+        type=str,
+        dest="field",
+        default="overpass time",
+        help="name of the field to report",
+    )
+    args = parser.parse_args()
+
+    get_single_field(**vars(args))
+
+def get_single_field(path, date, satellite, field):
+    df = pandas.read_csv(path, index_col=["date", "satellite"], parse_dates=["date"])
+    print(df.loc[date,satellite]["overpass time"])
+
+
+if __name__ == "__main__":
+    main()
