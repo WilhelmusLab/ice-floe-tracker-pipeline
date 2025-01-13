f94aab03b3994ad79c55a954a24d2615a8b547ea
John Gerrard Holland <john_holland1@brown.edu>
2025-01-13 21:21:26 +0000

    fix(examples): replace --icp and --fcp with START and END

--- workflow/example/util/template.py
+++ workflow/example/util/template.py
@@ -32,16 +32,16 @@ def _template(df, row_index):
         >>> df = pandas.read_csv(io.StringIO(csv), index_col="id")
 
         >>> print(_template(df, row_index="beaufort-sea-0"))
-        --icp 2020-09-05 --fcp 2020-09-08 --set BBOX="-2383879,-750000,-883879,750000" --set LOCATION="beaufort-sea"
+        --set START="2020-09-05" --set END="2020-09-08" --set BBOX="-2383879,-750000,-883879,750000" --set LOCATION="beaufort-sea"
 
         >>> print(_template(df, row_index="hudson-bay-0"))
-        --icp 2020-09-06 --fcp 2020-09-09 --set BBOX="-2795941,-3368686,-1295941,-1868686" --set LOCATION="hudson-bay"
-
+        --set START="2020-09-06" --set END="2020-09-09" --set BBOX="-2795941,-3368686,-1295941,-1868686" --set LOCATION="hudson-bay"
+        
     """
     r = df.loc[row_index]
     s = (
-        f"--icp {r.startdate} "
-        f"--fcp {r.enddate} "
+        f'--set START="{r.startdate}" '
+        f'--set END="{r.enddate}" '
         f'--set BBOX="{r.left_x},{r.lower_y},{r.right_x},{r.top_y}" '
         f'--set LOCATION="{r.location}"'
     )
