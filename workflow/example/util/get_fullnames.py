c160af48ca883b85bd5b6bc331c9254eecb3383c
John Gerrard Holland <john_holland1@brown.edu>
2025-01-13 21:12:24 +0000

    docs(examples): add rudimentary docstring to example utility file

--- workflow/example/util/get_fullnames.py
+++ workflow/example/util/get_fullnames.py
@@ -7,19 +7,26 @@
 # ///
 
 import pathlib
-from typing import Optional
+from typing import Optional, Annotated
 
 import pandas
 import typer
 
 
 def main(
-    datafile: pathlib.Path,
-    index_col: str,
-    start: Optional[int] = None,
-    stop: Optional[int] = None,
-    step: Optional[int] = None,
+    datafile: Annotated[pathlib.Path, typer.Argument(help="path to csv file")],
+    index_col: Annotated[str, typer.Argument(help="name of column to return")],
+    start: Annotated[
+        Optional[int], typer.Argument(help="initial row index to return")
+    ] = None,
+    stop: Annotated[
+        Optional[int], typer.Argument(help="final row index to return")
+    ] = None,
+    step: Annotated[
+        Optional[int], typer.Argument(help="size of steps between returned row indices")
+    ] = None,
 ):
+    """Print values from the `index_col` column in a CSV file, from row `start` to `stop` in steps of `step`."""
     f = pandas.read_csv(datafile)
     print("\n".join(f[index_col].values[start:stop:step]))
 
