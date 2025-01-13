b4b02450637ce923d5ba8323f82c5c7b72711c34
John Gerrard Holland <john_holland1@brown.edu>
2025-01-13 21:22:04 +0000

    fix(examples): add centroid lat and lon to code

--- workflow/example/util/template.py
+++ workflow/example/util/template.py
@@ -25,14 +25,14 @@ def _template(df, row_index):
     Examples:
 
         >>> import io, pandas
-        >>> csv = '''id,location,left_x,right_x,lower_y,top_y,startdate,enddate
-        ... beaufort-sea-0,beaufort-sea,-2383879,-883879,-750000,750000,2020-09-05,2020-09-08
-        ... hudson-bay-0,hudson-bay,-2795941,-1295941,-3368686,-1868686,2020-09-06,2020-09-09
+        >>> csv = '''id,location,left_x,right_x,lower_y,top_y,center_lat,center_lon,startdate,enddate
+        ... beaufort-sea-0,beaufort-sea,-2383879,-883879,-750000,750000,75.0,-135.0,2020-09-05,2020-09-08
+        ... hudson-bay-0,hudson-bay,-2795941,-1295941,-3368686,-1868686,60.0,-83.0,2020-09-06,2020-09-09
         ... '''
         >>> df = pandas.read_csv(io.StringIO(csv), index_col="id")
 
         >>> print(_template(df, row_index="beaufort-sea-0"))
-        --set START="2020-09-05" --set END="2020-09-08" --set BBOX="-2383879,-750000,-883879,750000" --set LOCATION="beaufort-sea"
+        --set START="2020-09-06" --set END="2020-09-09" --set CENTROID_LAT="60.0" --set CENTROID_LON="-83.0" --set BBOX="-2795941,-3368686,-1295941,-1868686" --set LOCATION="hudson-bay"
 
         >>> print(_template(df, row_index="hudson-bay-0"))
         --set START="2020-09-06" --set END="2020-09-09" --set BBOX="-2795941,-3368686,-1295941,-1868686" --set LOCATION="hudson-bay"
@@ -42,6 +42,8 @@ def _template(df, row_index):
     s = (
         f'--set START="{r.startdate}" '
         f'--set END="{r.enddate}" '
+        f'--set CENTROID_LAT="{r.center_lat}" '  
+        f'--set CENTROID_LON="{r.center_lon}" '  
         f'--set BBOX="{r.left_x},{r.lower_y},{r.right_x},{r.top_y}" '
         f'--set LOCATION="{r.location}"'
     )
