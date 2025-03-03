212e3fdd9749cfbd69e386ea39559c99a4f6fde0
John Gerrard Holland <john_holland1@brown.edu>
2025-03-03 20:17:23 +0000

    correct attribution of small and large floe settings

--- IFTPipeline.jl/test/_test-track.jl
+++ IFTPipeline.jl/test/_test-track.jl
@@ -45,6 +45,6 @@ tracked = deserialize(joinpath(temp, "labeled_floes.jls"))
 @test isfile(joinpath(temp, "labeled_floes.jls"))
 # these tests worked up to IceFloeTracker.jl 0.6.3, broke from 0.6.4
 # TODO: fix underlying problem, or remove this test set
-@test_skip nrow(tracked) == 18
-@test_skip maximum(tracked.ID) == 6
+@test nrow(tracked) == 18
+@test maximum(tracked.ID) == 6
 @test "passtime" in names(tracked)
