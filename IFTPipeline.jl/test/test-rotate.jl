958bc98a86649794b44dde30487422aad81ebe28
John Gerrard Holland <john_holland1@brown.edu>
2025-03-12 21:31:37 +0000

    rename rotation test set

--- IFTPipeline.jl/test/test-rotate.jl
+++ IFTPipeline.jl/test/test-rotate.jl
@@ -1,12 +1,12 @@
 using Dates
 
-@testset "tracker" begin
+@testset "rotation" begin
     data_dir = joinpath(@__DIR__, "test_inputs", "rotation")
     temp_dir = mkpath(joinpath(@__DIR__, "__temp__", "rotation-single"))
     @testset "normal case" begin
         results = IFTPipeline.get_rotation_single(;
-            input=joinpath(data_dir, "floes.tracked.satellite.csv"),
-            output=joinpath(temp_dir, "floes.tracked.satellite.rotation.csv"),
+            input=joinpath(data_dir, "floes.tracked.normal.csv"),
+            output=joinpath(temp_dir, "floes.tracked.normal.rotation.csv"),
         )
         @test nrow(results) == 24
     end
