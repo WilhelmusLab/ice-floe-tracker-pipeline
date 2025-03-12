6f79737d22707469e0e3894eddc019087fe654c2
John Gerrard Holland <john_holland1@brown.edu>
2025-03-12 21:10:05 +0000

    test: fix function calls

--- IFTPipeline.jl/test/test-rotate.jl
+++ IFTPipeline.jl/test/test-rotate.jl
@@ -2,27 +2,27 @@ using Dates
 
 @testset "tracker" begin
     data_dir = joinpath(@__DIR__, "test_inputs", "rotation")
-    temp_dir = mkpath(joinpath(@__DIR__, "__temp__", "rotation-single")),
+    temp_dir = mkpath(joinpath(@__DIR__, "__temp__", "rotation-single"))
     @testset "normal case" begin
-        results = IFTPipeline.get_rotation_single(
-            joinpath(data_dir, "floes.tracked.satellite.csv"),
-            joinpath(temp_dir, "floes.tracked.satellite.rotation.csv"),
+        results = IFTPipeline.get_rotation_single(;
+            input=joinpath(data_dir, "floes.tracked.satellite.csv"),
+            output=joinpath(temp_dir, "floes.tracked.satellite.rotation.csv"),
         )
         @test nrow(results) == 24
     end
 
     @testset "short case" begin
-        results = IFTPipeline.get_rotation_single(
-            joinpath(data_dir, "floes.tracked.short.csv"),
-            joinpath(temp_dir, "floes.tracked.short.rotation.csv"),
+        results = IFTPipeline.get_rotation_single(;
+            input=joinpath(data_dir, "floes.tracked.short.csv"),
+            output=joinpath(temp_dir, "floes.tracked.short.rotation.csv"),
         )
         @test nrow(results) == 2
     end
 
     @testset "synthetic case" begin
-        results = IFTPipeline.get_rotation_single(
-            joinpath(data_dir, "floes.tracked.synthetic.csv"),
-            joinpath(temp_dir, "floes.tracked.synthetic.rotation.csv"),
+        results = IFTPipeline.get_rotation_single(;
+            input=joinpath(data_dir, "floes.tracked.synthetic.csv"),
+            output=joinpath(temp_dir, "floes.tracked.synthetic.rotation.csv"),
         )
         @test nrow(results) == 6
         @test results[1, :theta_deg] == 0
