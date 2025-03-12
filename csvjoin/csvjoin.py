a5c51ccac91ccd5bd99e80da6a5eec881997ac74
John Gerrard Holland <john_holland1@brown.edu>
2025-03-12 14:32:41 +0000

    feat(csvjoin): add support for joining on ISO8601 timestamps

--- csvjoin/csvjoin.py
+++ csvjoin/csvjoin.py
@@ -2,6 +2,7 @@
 """Utility to join two csv files."""
 
 from enum import Enum
+from functools import partial
 import pathlib
 from typing import Annotated
 
@@ -10,6 +11,7 @@ import typer
 
 app = typer.Typer()
 
+
 class HOW(str, Enum):
     left = "left"
     right = "right"
@@ -17,30 +19,27 @@ class HOW(str, Enum):
     inner = "inner"
     cross = "cross"
 
+
 @app.command()
 def main(
-    left: Annotated[
-        pathlib.Path, typer.Argument(help="path to left csv file")
-    ],
-    right: Annotated[
-        pathlib.Path, typer.Argument(help="path to right csv file")
-    ],
-    on: Annotated[
-        str, typer.Argument(help="column on which to join")
-    ],
-    output: Annotated[
-        pathlib.Path, typer.Argument(help="path to output csv file")
-    ],
-    how: Annotated[
-        HOW,
-        typer.Option(help="type of join")
-    ]=HOW.left,
+    left: Annotated[pathlib.Path, typer.Argument(help="path to left csv file")],
+    right: Annotated[pathlib.Path, typer.Argument(help="path to right csv file")],
+    on: Annotated[str, typer.Argument(help="column on which to join")],
+    output: Annotated[pathlib.Path, typer.Argument(help="path to output csv file")],
+    how: Annotated[HOW, typer.Option(help="type of join")] = HOW.left,
+    on_is_utc: Annotated[
+        bool, typer.Option(help="`on` is a datetime which should be treated as UTC")
+    ] = False,
 ):
     """Join two csv files."""
-    left_df = pandas.read_csv(left, index_col=on)
-    right_df = pandas.read_csv(right, index_col=on)
+    read_kwargs = dict(index_col=on)
+    if on_is_utc:
+        read_kwargs["converters"] = {on: partial(pandas.to_datetime, utc=True)}
+    left_df = pandas.read_csv(left, **read_kwargs)
+    right_df = pandas.read_csv(right, **read_kwargs)
     output_df = left_df.join(right_df, on=on, how=how.value)
-    output_df.to_csv(output)
+    output_df.to_csv(output, date_format="%Y-%m-%dT%H:%M:%SZ")
+
 
 if __name__ == "__main__":
     app()
