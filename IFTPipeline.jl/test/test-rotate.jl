ab9974e1ced59883ea167996ef84c3959a0be174
John Gerrard Holland <john_holland1@brown.edu>
2025-04-11 14:45:55 +0000

    update testcases

--- IFTPipeline.jl/test/test-rotate.jl
+++ IFTPipeline.jl/test/test-rotate.jl
@@ -1,8 +1,9 @@
-using IFTPipeline: measure_rotation, get_rotation_shape_difference
+using IFTPipeline: measure_rotation
 
 @testset "rotation" begin
     data_dir = joinpath(@__DIR__, "test_inputs", "rotation")
-    temp_dir = mkpath(joinpath(@__DIR__, "__temp__", "rotation-single"))
+    temp_dir = mkpath(joinpath(@__DIR__, "__temp__", "rotation"))
+
     @testset "normal case" begin
         results = measure_rotation(;
             input=joinpath(data_dir, "floes.tracked.normal.csv"),
@@ -17,5 +18,16 @@ using IFTPipeline: measure_rotation, get_rotation_shape_difference
             output=joinpath(temp_dir, "floes.tracked.short.rotation.csv"),
         )
         @test nrow(results) == 2
+    end
+
+    @testset "synthetic case" begin
+        results = measure_rotation(;
+            input=joinpath(data_dir, "floes.tracked.synthetic.csv"),
+            output=joinpath(temp_dir, "floes.tracked.synthetic.rotation.csv"),
+        )
+        @test nrow(results) == 6
+        @test results[1, :theta_deg] == 0
+        @test results[2, :theta_deg] == 0
+        @test -47 < results[6, :theta_deg] < -43  # should be -45º
     end
 end
