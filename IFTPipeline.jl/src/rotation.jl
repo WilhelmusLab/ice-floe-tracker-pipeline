d7e4aba8e98a509abd5b3429d0d76f5d978b232d
John Gerrard Holland <john_holland1@brown.edu>
2025-04-11 14:40:49 +0000

    update docstring

--- IFTPipeline.jl/src/rotation.jl
+++ IFTPipeline.jl/src/rotation.jl
@@ -5,25 +5,32 @@ using CSV
 """
 Make a CSV of pairwise rotations between floes detected on adjacent days. 
 
-Loads the floes from the `input` CSV file, and uses the columns:
-- `floe` ID
-- `satellite` name
-- `mask` – the binary mask (choose a column using argument `mask_column`)
-- `passtime` in ISO8601 format (with trailing Z or +00:00), e.g. 2022-09-11T09:21:00+00:00  (choose a column using argument `time_column`)
+Loads the floes from the `input` CSV file. Columns required:
+- floe `ID`
+- `mask` – the binary mask
+- `passtime` in ISO8601 format:
+  Needs trailing `Z` or `+00:00`, e.g. `2022-09-11T09:21:00Z` or `2022-09-11T09:21:00+00:00`
 
+Other columns often included:
+- `satellite` name
 
 Returns a CSV with one row per floe comparison. 
 In the following, `i=1` means the earlier observation, `i=2` the later.
 
 Columns returned:
-- `ID` of the floe
-- Angle measures `theta_<deg,rad>` – angle between floe image in degrees or radians
+- Angle measures:
+  - `theta_deg` – angle between floe image in degrees
+  - `theta_rad` – angle between floe image in radians
 - Time measurements:
-  - `passtime<i>` – which UTC time measurement `i`'s overpass occurred
-  - `delta_time_sec` – number of seconds between overpass in the two measurements
-  - `omega_<deg,rad>_per_<sec,hour,day>` – mean angular velocity of rotation in degrees or radians per second hour or day.
-- Any columns listed in `additional_columns` will also be included like `<name><i>` in the output
-  - `mask<i>` – the binary mask used for the measurement is always last.
+  - `dt_sec` – number of seconds between overpass in the two measurements
+  - `omega_rad_per_sec` – angular velocity of rotation in radians per second.\
+  - `omega_rad_per_day` – angular velocity of rotation in radians per day.
+  - `omega_deg_per_day` – angular velocity of rotation in radians per day.
+- Metadata: all of the columns from from the input with the column name suffix `i` including:
+  - `ID<i>`
+  - `mask<i>`
+  - `passtime<i>`
+  - if available: `satellite<i>`.
 """
 function measure_rotation(;
     input::String, output::String, id_column=:ID, image_column=:mask, time_column=:passtime
