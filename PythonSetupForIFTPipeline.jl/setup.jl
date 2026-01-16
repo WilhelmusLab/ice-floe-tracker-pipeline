8763d6553745d5d88fd9c2c0f156314008400516
John Gerrard Holland <john_holland1@brown.edu>
2026-01-16 18:32:36 +0000

    remove PythonSetup scripts

--- PythonSetupForIFTPipeline.jl/setup.jl
+++
@@ -1,9 +0,0 @@
-# Instantiate all the dependencies
-using Pkg
-
-# Force PyCall to use the Conda version on Linux.
-ENV["PYTHON"] = ""
-
-# Build PyCall with the new conda environment
-Pkg.add("PyCall")
-Pkg.build("PyCall")
